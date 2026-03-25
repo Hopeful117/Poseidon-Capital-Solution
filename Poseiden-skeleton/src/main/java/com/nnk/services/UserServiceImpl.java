3c92a522371064638a978bcf7061ce05c3ae8e14
ludo <ludovic.brot@gmail.com>
2026-03-25 19:57:02 +0100

    fix loginPage

--- Poseiden-skeleton/src/main/java/com/nnk/services/UserServiceImpl.java
+++ Poseiden-skeleton/src/main/java/com/nnk/services/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.nnk.services;
 import com.nnk.domain.User;
 import com.nnk.exceptions.EntityNotFoundException;
 import com.nnk.repositories.UserRepository;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -13,10 +12,11 @@ public class UserServiceImpl extends AbstractCrudService<User> {
     protected UserServiceImpl(UserRepository repository) {
         super(repository);
     }
+
     @Override
-    public void create (User user){
+    public void create(User user) {
         Assert.notNull(user, "Objet is null");
-        Assert.isNull(user.getId() , "Id can not be defined");
+        Assert.isNull(user.getId(), "Id can not be defined");
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         user.setPassword(encoder.encode(user.getPassword()));
 
@@ -24,14 +24,14 @@ public class UserServiceImpl extends AbstractCrudService<User> {
     }
 
     @Override
-    public void update (User user){
+    public void update(User user) {
         Assert.notNull(user, "Objet is null");
-        Assert.notNull(user.getId() , "Id can not be null");
+        Assert.notNull(user.getId(), "Id can not be null");
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         user.setPassword(encoder.encode(user.getPassword()));
-        User oldUser= repository.findById(user.getId())
-                .orElseThrow(()-> new EntityNotFoundException("Id not found"));
-        User newUser=oldUser.update(user);
+        User oldUser = repository.findById(user.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Id not found"));
+        User newUser = oldUser.update(user);
 
         repository.save(newUser);
     }
